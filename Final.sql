7259ec230ce37ae40bccb6d7cf5400cffe8438d6
khenson1 <86619648+khenson1@users.noreply.github.com>
2021-11-21 23:04:51 -0600

    Updating the final SQL code

--- Final.sql
+++ Final.sql
@@ -1,44 +1,57 @@
 --Create Main Table
-CREATE TABLE Final_Project_AHM (
-  zpid INT NOT NULL,
-  zipcode INT NOT NULL,
-  latitude decimal NOT NULL,
-  longitude decimal NOT NULL,
-	yearBuilt int not null,
-  latestPrice INT not NULL,
-	latest_saleyear int not null,
-  livingAreSQFt Int not null,
-	numofBathrooms decimal not null,
-	numofBedrooms decimal not null,
-	homeAge_years int not null
+CREATE TABLE Austin_Housing_Market (
+  	zpid INT NOT NULL,
+  	streetAddress VARCHAR NOT NULL,
+  	zipcode INT NOT NULL,
+	yearBuilt INT NOT NULL,
+  	latestPrice INT NOT NULL,
+	latest_salemonth INT NOT NULL,
+	latest_saleyear INT NOT NULL,
+	latitude DECIMAL NOT NULL,
+  	longitude DECIMAL NOT NULL,
+  	livingAreSQFt Int NOT NULL,
+	numofBathrooms DECIMAL NOT NULL,
+	numofBedrooms DECIMAL NOT NULL,
+	homeAge_years INT NOT NULL,
+	price_sqft INT NOT NULL
 	);
 	
---Create Housing Table
-	select fp.zpid,
-		fp.yearBuilt,
-		fp.homeAge_years,
-		fp.livingAreSQFt,
-		fp.numofBathrooms,
-		fp.numofBedrooms
-	into Housing
-	from final_project_ahm as fp
+--Create Housing Features Table
+	select ahm.streetAddress,
+		ahm.yearBuilt,
+		ahm.homeAge_years,
+		ahm.livingAreSQFt,
+		ahm.numofBathrooms,
+		ahm.numofBedrooms
+	into Features
+	from Austin_Housing_Market as ahm
 	;
-
---Create Location Table by a right join
-	select housing.zpid,
-		fp.zipcode,
-		fp.latitude,
-		fp.longitude
+	
+--Create Housing Location Table 
+	select ahm.zpid,
+		ahm.zipcode,
+		ahm.latitude,
+		ahm.longitude
 	into Location
-	from final_project_ahm as fp
-	right join housing
-	on fp.zip = housing.zpid
+	from Austin_Housing_Market as ahm
 	;
-
---Create Price_year Table
-	select fp.zpid,
-		fp.latest_saleyear,
-		fp.latestPrice
-	into Price_year
-	from final_project_ahm as fp
+	
+--Create Time of sale Table with a right join
+	select lo.zpid,
+		ahm.latest_salemonth,
+		ahm.latest_saleyear
+	into saletime
+	from Austin_Housing_Market as ahm
+	right join location as Lo
+	on ahm.zpid = Lo.zpid
+	;
+	
+--Create Price Table with a right join
+	select lo.zpid,
+		ahm.streetAddress,
+		ahm.latestPrice
+	into Price
+	from Austin_Housing_Market as ahm
+	right join location as lo
+	on ahm.zpid = lo.zpid
 	;
